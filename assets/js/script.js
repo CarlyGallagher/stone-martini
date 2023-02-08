bcddafe5b1131f94842cf4c333048c49f1ce40ef
rambriz91 <rambriz91@gmail.com>
2023-02-07 21:00:08 -0800

    cleaned up script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,14 @@
 // Image API
 var drinkImg = document.getElementById('drink-img')
 var ninjaKey = 'mbA0oOq8BxNEPSK4vFbW7Q==IfwPuDu0oCLy5XEX'
-
+var result;
 
 
 getDrink()
 
 function getDrink() {
-
-    //var drinkImgAPI = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s='+result;
-    var drinkImgAPI = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-    //var result;
+    var result = 'margarita'
+    var drinkImgAPI = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=' + result;
 
     fetch(drinkImgAPI)
         .then(function (response) {
@@ -20,12 +18,12 @@ function getDrink() {
             console.log(data)
             document.getElementById('drink-img').src = data.drinks[0].strDrinkThumb
         })
-
+        getRecipe()
 }
 
 function getRecipe() {
-    var name = 'margarita'
-    var recipeAPI ='https://api.api-ninjas.com/v1/cocktail?name=' +name;
+        result = 'margarita'
+    var recipeAPI = 'https://api.api-ninjas.com/v1/cocktail?name=' + result;
 
     fetch(recipeAPI, { headers: { 'X-Api-Key': ninjaKey } })
         .then(function (response) {
@@ -35,10 +33,9 @@ function getRecipe() {
             console.log(data)
             document.getElementById('cocktail-name').textContent = data[0].name;
             for (var i = 0; i < data[0].ingredients.length; i++)
-            document.getElementById('recipe-ingred-' +i+'').textContent = data[0].ingredients[i];
+                document.getElementById('recipe-ingred-' + i + '').textContent = data[0].ingredients[i];
             document.getElementById('instruction').textContent = data[0].instructions;
-            
+
         })
 
 }
-getRecipe()
